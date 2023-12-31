1d4c294080bbd92f75f68650b3152f8b9beca0de
bhavesh <bhaveshdaswani93@gmail.com>
2023-12-31 18:30:23 +0530

    Added text for preivew component

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,28 @@ export default function Home() {
         width={120}
         height={120}
       />
+      <div className='bg-white p-4'>
+        <p className='uppercase tracking-[.35rem] text-xs text-dark-gray-blue'>Perfume</p>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+        <p>
+          <span>
+            $149.99
+          </span>
+          <span>169.99</span>
+
+          
+        </p>
+        <button>
+            <Image 
+              src={'/assets/icons/icon-cart.svg'}
+              width={15}
+              height={16}
+              alt='Add to cart'
+            />
+            Add to Cart
+          </button>
+      </div>
     </main>
   )
 }
